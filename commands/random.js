d6ccebe8246b1824120c7ae159375e73155ccc57
Tim <46400905+timfernix@users.noreply.github.com>
2024-08-04 02:10:03 +0200

    ✨ Updated to v2.9 - Implementation of feedback command
    - Implementation of /feedback
    - Rewrite of  /random
    - Advanced timezones in /timestamp
    - Imported Axios for future commands

--- commands/random.js
+++ commands/random.js
@@ -1,7 +1,5 @@
-const { timeStamp } = require("console");
-const { SlashCommandBuilder, EmbedBuilder, time, Guild } = require("discord.js");
-const { champions } = require("../data/champions");
-const data = require("../data/champions").champions;
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const data = require("../data/champions.js").champions;
 const { GUILD_ID } = process.env;
 
 module.exports = {
@@ -23,14 +21,12 @@ module.exports = {
     ),
 
     async execute(interaction){
+        const PickedRole = interaction.options.getString("role");
         const reducedChampions = [];
         const CRole = [];
-        const PickedRole = interaction.options.getString("role");
         CRole.push(PickedRole);
 
-        if(interaction.guildId === GUILD_ID){
-        //--------------------------------------------------------------------------------------
-        if(CRole.length) {
+    if(CRole.length) {
             for(let role of CRole) {  
                 data.reduce((champions, cur, index) => {
                     if(cur.role.includes(role)) {
@@ -43,27 +39,43 @@ module.exports = {
 
         const randomChampion =
         reducedChampions[Math.floor(Math.random() * reducedChampions.length)];
-        
-        const percentage = Math.round((1/reducedChampions.length) * 100)
+        const percentage = ((1/reducedChampions.length) * 100).toFixed(2)
         const picks = Math.round(randomChampion.wins + randomChampion.losses)
 
         let winrate = Math.round((randomChampion.wins / picks) * 100)
         if(!winrate){
             winrate = 0
         }
-        
-        const embed = new EmbedBuilder()
-        .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
-        .setURL(`https://u.gg/lol/champions/${randomChampion.name}`)
-        .setDescription(`You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**! \n\n **${randomChampion.name}** has been rolled **${picks} time(s)** before (in a tournament) \n and won **${randomChampion.wins} game(s)** of those played. (**Winrate: ${winrate}%**)`)
-        .setColor(0x0099FF)
-        .setTimestamp()
-        .setFooter({text: "Generated: "})
-        //.setThumbnail(`${randomChampion.icon}`);
-        .setImage(`${randomChampion.splash}`);
 
-        return interaction.reply({ embeds: [embed] });
-    }else {
+        if(interaction.guildId === GUILD_ID){
+            const championImage = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+randomChampion.smoothName+"_0.jpg";
+            const embedDescription = `You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**! \n\n **${randomChampion.name}** has been rolled **${picks} time(s)** before (in a tournament) \n and won **${randomChampion.wins} game(s)** of those played. (**Winrate: ${winrate}%**)`
+            const embed = new EmbedBuilder()
+            .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
+            .setURL(`https://u.gg/lol/champions/${randomChampion.smoothName}`)
+            .setDescription(`${embedDescription}`)
+            .setColor(0x0099FF)
+            .setTimestamp()
+            .setFooter({text: "Generated: "})
+            .setImage(`${championImage}`);
+
+            return interaction.reply({ embeds: [embed] });
+    }
+         else if(interaction.guildId != GUILD_ID){
+            const championImage = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+randomChampion.smoothName+"_0.jpg";
+            const embedDescription = `You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**!`
+            const embed = new EmbedBuilder()
+            .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
+            .setURL(`https://u.gg/lol/champions/${randomChampion.smoothName}`)
+            .setDescription(`${embedDescription}`)
+            .setColor(0x0099FF)
+            .setTimestamp()
+            .setFooter({text: "Generated: "})
+            .setImage(`${championImage}`);
+
+            return interaction.reply({ embeds: [embed] });
+    }
+    else {
         const randomChampion = data[Math.floor(Math.random() * data.length)];
 
         const percentage = Math.floor((1/data.length) * 100)
@@ -73,81 +85,35 @@ module.exports = {
         if(!winrate){
             winrate = 0
         }
-
-        const embed = new EmbedBuilder()
-        .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
-        .setURL(`https://u.gg/lol/champions/${randomChampion.name}`)
-        .setDescription(`You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**! \n\n **${randomChampion.name}** has been rolled **${picks} time(s)** before (in a tournament) \n and won **${randomChampion.wins} game(s)** of those played. (**Winrate: ${winrate}%**)`)
-        .setColor(0x0099FF)
-        .setTimestamp()
-        .setFooter({text: "Generated: "})
-        //.setThumbnail(`${randomChampion.icon}`);
-        .setImage(`${randomChampion.splash}`);
-
-        return interaction.reply({ embeds: [embed] });
-        }
-        //--------------------------------------------------------------------------------------
     }
-    else if(interaction.guildId != GUILD_ID) {
-                //--------------------------------------------------------------------------------------
+        if(interaction.guildId === GUILD_ID){
+            const championImage = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+randomChampion.smoothName+"_0.jpg";
+            const embedDescription = `You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**! \n\n **${randomChampion.name}** has been rolled **${picks} time(s)** before (in a tournament) \n and won **${randomChampion.wins} game(s)** of those played. (**Winrate: ${winrate}%**)`
+            const embed = new EmbedBuilder()
+            .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
+            .setURL(`https://u.gg/lol/champions/${randomChampion.smoothName}`)
+            .setDescription(`${embedDescription}`)
+            .setColor(0x0099FF)
+            .setTimestamp()
+            .setFooter({text: "Generated: "})
+            .setImage(`${championImage}`);
 
-                if(CRole.length) {
-                    for(let role of CRole) {  
-                        data.reduce((champions, cur, index) => {
-                            if(cur.role.includes(role)) {
-                                if(!reducedChampions.includes(cur)) {
-                                 reducedChampions.push(cur);
-                            }
-                        }
-                    });
-                }
-        
-                const randomChampion =
-                reducedChampions[Math.floor(Math.random() * reducedChampions.length)];
-                
-                const percentage = Math.round((1/reducedChampions.length) * 100)
-                const picks = Math.round(randomChampion.wins + randomChampion.losses)
-        
-                let winrate = Math.round((randomChampion.wins / picks) * 100)
-                if(!winrate){
-                    winrate = 0
-                }
-                
-                const embed = new EmbedBuilder()
-                .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
-                .setURL(`https://u.gg/lol/champions/${randomChampion.name}`)
-                .setDescription(`You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**!`)
-                .setColor(0x0099FF)
-                .setTimestamp()
-                .setFooter({text: "Generated: "})
-                //.setThumbnail(`${randomChampion.icon}`);
-                .setImage(`${randomChampion.splash}`);
-        
-                return interaction.reply({ embeds: [embed] });
-            }else {
-                const randomChampion = data[Math.floor(Math.random() * data.length)];
-        
-                const percentage = Math.floor((1/data.length) * 100)
-                const picks = Math.round(randomChampion.wins + randomChampion.losses)
-        
-                let winrate = Math.round((randomChampion.wins / picks) * 100)
-                if(!winrate){
-                    winrate = 0
-                }
-        
-                const embed = new EmbedBuilder()
-                .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
-                .setURL(`https://u.gg/lol/champions/${randomChampion.name}`)
-                .setDescription(`You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**!`)
-                .setColor(0x0099FF)
-                .setTimestamp()
-                .setFooter({text: "Generated: "})
-                //.setThumbnail(`${randomChampion.icon}`);
-                .setImage(`${randomChampion.splash}`);
-        
-                return interaction.reply({ embeds: [embed] });
-                }
-                //--------------------------------------------------------------------------------------
+            return interaction.reply({ embeds: [embed] });
+
+        };
     }
-    }
-}
+         else if(interaction.guildId != GUILD_ID){
+            const championImage = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+randomChampion.smoothName+"_0.jpg";
+            const embedDescription = `You rolled **${randomChampion.name}** for **${PickedRole}** with a chance of **~${percentage}%**!`
+            const embed = new EmbedBuilder()
+            .setTitle(`${randomChampion.name} - ${randomChampion.title}`)
+            .setURL(`https://u.gg/lol/champions/${randomChampion.smoothName}`)
+            .setDescription(`${embedDescription}`)
+            .setColor(0x0099FF)
+            .setTimestamp()
+            .setFooter({text: "Generated: "})
+            .setImage(`${championImage}`);
+
+            return interaction.reply({ embeds: [embed] });
+        };
+}}
